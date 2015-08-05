3e5acb15938f3915690a0d71f217951b9d57c540
root <598428034@qq.com>
2015-08-05 20:35:49 +0800

    main return value test

--- int_ret_with_void_foo.c
+++ int_ret_with_void_foo.c
@@ -1,6 +1,10 @@
 //http://www.delorie.com/djgpp/v2faq/faq22_25.html
 //
 //http://www.tldp.org/LDP/LGNET/84/hawk.html
+//
+//first time ./a.out and echo $?,we will see the echo is 0, so the gcc change the main?
+// but when we run it again it return 15,PS:GDB return value is octal (15 = 17(octal)
+// test on ubuntu15.04, the return value is always 15
 #include <stdio.h>
 
 void foo()// this is the main() function
